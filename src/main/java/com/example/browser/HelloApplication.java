ad2e5377a91ef4f922e653bc588410d8d0424fa0
bam10112002 <bam10112002@gmail.com>
2022-11-06 21:51:34 +0300

    add icon for program

--- src/main/java/com/example/browser/HelloApplication.java
+++ src/main/java/com/example/browser/HelloApplication.java
@@ -2,7 +2,10 @@ package com.example.browser;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import java.io.File;
 
 public class HelloApplication extends Application {
 //    private static Logger log = Logger.getLogger(HelloApplication.class.getName());
@@ -10,6 +13,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) {
         UI ui = new UI();
         Scene scene = new Scene(ui.getRoot());
+        stage.getIcons().add(new Image(new File("src/main/resources/favicon.png").toURI().toString()));
         stage.setMinHeight(700);
         stage.setMinWidth(1000);
         stage.setTitle("Browser");
